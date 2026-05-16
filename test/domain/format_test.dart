b3f4c0eaae35b3f3fc0e573aa28298b5671cf7ed
CHENWEI PAN <Marxppppp@gmail.com>
2026-05-16 09:52:58 +0800

    feat: add duration + pace formatting helpers

---
+++ test/domain/format_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:freshloop/domain/format.dart';
+
+void main() {
+  group('formatDuration', () {
+    test('mm:ss under an hour', () {
+      expect(formatDuration(0), '00:00');
+      expect(formatDuration(65), '01:05');
+    });
+    test('h:mm:ss at/over an hour', () {
+      expect(formatDuration(3725), '1:02:05');
+    });
+  });
+  group('formatPace', () {
+    test("min'sec\" per km", () {
+      expect(formatPace(1000, 300), "5'00\"");
+      expect(formatPace(2000, 600), "5'00\"");
+    });
+    test('placeholder when distance is ~0', () {
+      expect(formatPace(0, 120), "--'--\"");
+    });
+  });
+}
